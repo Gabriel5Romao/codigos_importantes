a28d84b1b8c7ed5f309af589590fdc276fec253c
Gabriel Romão <gabriel.s.romao@outlook.com>
2021-01-02 10:23:39 -0300

    Create comprehensions.py

---
+++ comprehensions.py
@@ -0,0 +1,36 @@
+"""
+List Comprehensions:
+  Maneira rápida e elegante para criação de listas.
+  
+  1° forma:
+    [ elemento_com_operacao for elemento in iterable ]
+    
+  2° forma:
+    [ elemento_com_operacao for elemento in iterable if condicao ]
+    
+  3° forma:
+    [ elemento_com_operacao if condicao else outra_operacao for elemento in iterable ]
+  
+  Aninhadas:
+    [ [ list_comprehension1 ] for x in interable ]
+    Na situaçãoa acima o "for x in iterable" é executado primeiro.
+"""
+
+algarismos_quadrado = [ i**2 for i in range(0,10)]
+
+pares = [ j for j in range(1,10) if j%2==0 ]
+impares = [ m for m in range(1,10) if m%2!=0 ]
+
+lista_louca = [ x ** 2 if x%2==0 else x /2 - 1 for x in range(1,10) ] 
+# Se x é par eleva ao quadrado, caso contrário divida por 2 e subtraia 1
+
+"""
+Dict Comprehensions:
+  Sintaxe:
+    { chave:valor for chave,valor in interable }
+  
+""'
+
+
+
+
